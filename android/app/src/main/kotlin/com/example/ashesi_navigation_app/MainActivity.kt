66cc7fd43b0c0459f1320b3c0ede94eaf7dac7a0
cyrilk7 <cyrilkujar@gmail.com>
2024-06-22 18:16:36 +0000

    s

--- android/app/src/main/kotlin/com/example/ashesi_navigation_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ashesi_navigation_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/models/route_model.dart
+++
@@ -1,6 +0,0 @@
-class RoutePoint {
-  final double latitude;
-  final double longitude;
-
-  RoutePoint({required this.latitude, required this.longitude});
-}
